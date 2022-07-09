231d6703e9e5a9b3ef6d65771191610ff60d5a91
sarawut-pcru <sarawut.aou@pcru.ac.th>
2022-07-09 11:42:42 +0700

    add data

--- application/core/path.php
+++ application/core/path.php
@@ -1,5 +1,29 @@
 <?php
+$title_path = 'แบบบันทึกตรวจสุขภาพ';
 
-    $title_path = 'แบบบันทึกตรวจสุขภาพ';
+function select_data($type = '')
+{
+    require_once 'data_utllities.php';
+    if ($type = 'title') {
+        $data = '<option value="">------เลือกคำนำหน้า------</option>';
+        foreach ($title_name as $key => $val) {
+            $data += '<option value="' . $key . '">' . $val . '</option>';
+        }
+        return $data;
+    }
 
-    
+    if ($type = 'cigarate') {
+        $data = '<option value="">------เลือกชนิดบุหรี่------</option>';
+        foreach ($cigarate as $key => $val) {
+            $data += '<option value="' . $key . '">' . $val . '</option>';
+        }
+        return $data;
+    }
+    if ($type = 'alcohol') {
+        $data = '<option value="">------เลือกชนิดเหล้าสี------</option>';
+        foreach ($alcohol as $key => $val) {
+            $data += '<option value="' . $key . '">' . $val . '</option>';
+        }
+        return $data;
+    }
+}
